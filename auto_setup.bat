b00e79bcbcc4645014ccb63310bbcbff3dcd9e2b
AndreikaKopeika <151951862+AndreikaKopeika@users.noreply.github.com>
2024-09-15 17:34:38 +0300

    Create auto_setup.bat

---
+++ auto_setup.bat
@@ -0,0 +1,7 @@
+echo staring automatic setup!
+git clone https://github.com/AndreikaKopeika/Discord-openai.git
+cd Discord-openai
+echo staring setup.bat!
+start setup.bat
+echo setup done! starting bot...
+python bot.py
